3d0065b7b72aef536ffa20353ba6b1fa26939d8f
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-02-18 15:30:11 +0300

    18.02.2026

--- anfisa_for_friends/ice_cream/models.py
+++ anfisa_for_friends/ice_cream/models.py
@@ -4,35 +4,53 @@ from core.models import PublishedModel
 
 
 class Category(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
-    output_order = models.PositiveSmallIntegerField(default=100)
+    title = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+    output_order = models.PositiveSmallIntegerField(
+        default=100, verbose_name="Порядок отображения"
+    )
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "Категории"
 
 
 class Topping(PublishedModel):
-    title = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=64, unique=True)
+    title = models.CharField('Название', max_length=256)
+    slug = models.SlugField('Слаг', max_length=64, unique=True)
+
+    class Meta:
+        verbose_name = "топпинг"
+        verbose_name_plural = "Топпинги"
 
 
 class Wrapper(PublishedModel):
-    title = models.CharField(max_length=256)
+    title = models.CharField('Название', max_length=256)
+
+    class Meta:
+        verbose_name = "обёртка"
+        verbose_name_plural = "Обёртки"
 
 
 class IceCream(PublishedModel):
-    title = models.CharField(max_length=256)
-    description = models.TextField()
+    title = models.CharField('Название', max_length=256)
+    description = models.TextField('Описание', blank=True)
     wrapper = models.OneToOneField(
         Wrapper,
         on_delete=models.SET_NULL,
-        related_name='ice_cream',
+        related_name="ice_cream",
         null=True,
         blank=True,
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.CASCADE,
-        related_name='ice_creams',
+        related_name="ice_creams",
+        verbose_name='Категория',
     )
     toppings = models.ManyToManyField(Topping)
-    is_on_main = models.BooleanField(default=False)
+    is_on_main = models.BooleanField('На главную', default=False)
 
+    class Meta:
+        verbose_name = "мороженое"
+        verbose_name_plural = "Мороженое"
