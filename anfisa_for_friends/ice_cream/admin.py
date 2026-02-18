3e732c92df66a1c2a24c8329737ad3a07987960e
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-02-18 23:17:03 +0300

    донастроил админ зону

--- anfisa_for_friends/ice_cream/admin.py
+++ anfisa_for_friends/ice_cream/admin.py
@@ -4,8 +4,47 @@ from django.contrib import admin
 # Из модуля models импортируем модель Category...
 from .models import Category, Topping, Wrapper, IceCream
 
+
+admin.site.empty_value_display = 'Не задано'
+
+# Создаём класс, в котором будем описывать настройки админки:
+# ice_cream/admin.py
+class IceCreamInline(admin.StackedInline):
+    model = IceCream
+    extra = 0
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = (
+        IceCreamInline,
+    )
+    list_display = (
+        'title',
+    )
+
+class IceCreamAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'is_published',
+        'is_on_main',
+        'category',
+        'wrapper'
+    )
+    list_editable = (
+        'is_published',
+        'is_on_main',
+        'category'
+    )
+    search_fields = ('title',)
+    list_filter = ('category',)
+    list_display_links = ('title',)
+    empty_value_display = 'Не задано'
+    filter_horizontal = ('toppings',)
+
+
+
 # ...и регистрируем её в админке:
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Topping)
 admin.site.register(Wrapper)
-admin.site.register(IceCream)
+admin.site.register(IceCream, IceCreamAdmin)
