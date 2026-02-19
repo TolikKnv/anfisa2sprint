88e70a8a37e4567ca45e87e6399134c1366391d1
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-02-19 22:35:58 +0300

    работал с фильтрацией и выводом информации из бд

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
+# def index(request):
+#     template_name = 'homepage/index.html'
+#     # Заключаем вызов методов в скобки
+#     # (это стандартный способ переноса длинных строк в Python);
+#     # каждый вызов пишем с новой строки, так проще читать код:
+#     ice_cream_list = IceCream.objects.values(
+#             'id', 'title', 'description'
+#         # Верни только те объекты, у которых в поле is_on_main указано True:
+#         ).filter(is_on_main=True)
+#     context = {
+#         'ice_cream_list': ice_cream_list,
+#     }
+#     return render(request, template_name, context)
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+            'id', 'title', 'description'
+        # Исключи те объекты, у которых is_published=False:
+        ).exclude(is_published=False)
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
