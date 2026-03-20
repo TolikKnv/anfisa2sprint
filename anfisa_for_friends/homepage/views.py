30ed6f8219661fc73958e15abcdeddadc61d3776
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-03-20 15:20:34 +0300

    сука вспомнить всё

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,28 +1,17 @@
+from django.db.models import Q
+
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
 
-# def index(request):
-#     template_name = 'homepage/index.html'
-#     # Заключаем вызов методов в скобки
-#     # (это стандартный способ переноса длинных строк в Python);
-#     # каждый вызов пишем с новой строки, так проще читать код:
-#     ice_cream_list = IceCream.objects.values(
-#             'id', 'title', 'description'
-#         # Верни только те объекты, у которых в поле is_on_main указано True:
-#         ).filter(is_on_main=True)
-#     context = {
-#         'ice_cream_list': ice_cream_list,
-#     }
-#     return render(request, template_name, context)
 
 def index(request):
-    template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values(
-            'id', 'title', 'description'
-        # Исключи те объекты, у которых is_published=False:
-        ).exclude(is_published=False)
+    template_name = "homepage/index.html"
+    ice_cream_list = IceCream.objects.values("id", "title", "description").filter(
+        (Q(is_on_main=True) & Q(is_published=True))
+        | Q(title__contains="пломбир") & Q(is_published=True)
+    )
     context = {
-        'ice_cream_list': ice_cream_list,
+        "ice_cream_list": ice_cream_list,
     }
     return render(request, template_name, context)
