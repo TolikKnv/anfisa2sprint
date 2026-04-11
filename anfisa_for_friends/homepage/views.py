26c63c8cce9205645a42c35670a9f59853d4f5e6
Anatoliy Konovalov <anatoliykonovalov2008@gmail.com>
2026-04-11 18:10:03 +0300

    работа с фильтрацией и выводом данных

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -7,11 +7,31 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = "homepage/index.html"
-    ice_cream_list = IceCream.objects.values("id", "title", "description").filter(
-        (Q(is_on_main=True) & Q(is_published=True))
-        | Q(title__contains="пломбир") & Q(is_published=True)
-    )
+    ice_cream_list = IceCream.objects.values(
+    'id', 'title', 'description'
+    ).filter(
+    is_published=True, is_on_main=True
+    ).order_by('title')[1:4]
     context = {
         "ice_cream_list": ice_cream_list,
     }
     return render(request, template_name, context)
+
+# from django.shortcuts import render
+
+# from ice_cream.models import Category, IceCream
+
+# def index(request):
+#     template_name = 'homepage/index.html'
+#     categories = Category.objects.values(
+#         'id', 'output_order', 'title'
+#     ).order_by(
+#         # Сортируем записи по значению поля output_order,
+#         # а если значения output_order у каких-то записей равны -
+#         # сортируем эти записи по названию в алфавитном порядке.
+#         'output_order', 'title'
+#     )
+#     context = {
+#         'categories': categories
+#     }
+#     return render(request, template_name, context)
